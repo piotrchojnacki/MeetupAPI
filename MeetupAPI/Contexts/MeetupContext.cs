c79cc2d603f0bd89a1d85634e2526e5fe37cc008
piotrchojnacki <piotr.t.chojnacki@gmail.com>
2021-02-13 15:14:26 +0100

    [*][pc] Add MeetupContext and Cleaning

---
+++ MeetupAPI/Contexts/MeetupContext.cs
@@ -0,0 +1,31 @@
+﻿using MeetupAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetupAPI.Contexts
+{
+    public class MeetupContext : DbContext
+    {
+        private string _connectionString = "Data Source=DESKTOP-34B67NC;Initial Catalog=MeetupDB;Integrated Security=True";
+
+        public DbSet<Meetup> Meetups { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Lecture> Lectures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Meetup>()
+                .HasOne(m => m.Location)
+                .WithOne(l => l.Meetup)
+                .HasForeignKey<Location>(l => l.MeetupId);
+
+            modelBuilder.Entity<Meetup>()
+                .HasMany(m => m.Lectures)
+                .WithOne(l => l.Meetup);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
